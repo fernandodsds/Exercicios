dbb469f24990c240fc60833bf2796220bc3abf1e
u16269 <u16269@Lapa24.academico.cotuca.unicamp.br>
2018-03-27 18:18:37 -0300

    Exercicio resolução de expressoes

--- src/testePerf.java
+++ src/testePerf.java
@@ -4,18 +4,25 @@ import fila.Fila2;
 public class testePerf {
 	
 	public static void main(String[] args) {
-    try {
-		Fila2<Integer> a = new Fila2<Integer>(10000000);
-		for (int i = 0; i < 10000000; i++) {
-			a.guardeUmItem(new Integer(i));
+//    try {
+//		Fila2<Integer> a = new Fila2<Integer>(10000000);
+//		for (int i = 0; i < 10000000; i++) {
+//			a.guardeUmItem(new Integer(i));
+//		}
+//		for (int i = 0; i < 10000000; i++) {
+//			a.jogueForaUmItem();
+//		}
+//	} catch (Exception e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+		
+		try {
+			String exp = "10+(2*3-4)^(3+6)/2";
+			algebraDoMaligno.primeiraParte(algebraDoMaligno.quebra(exp));
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
-		for (int i = 0; i < 10000000; i++) {
-			a.jogueForaUmItem();
-		}
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
 	}
 
 }
